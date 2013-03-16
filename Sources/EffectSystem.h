6a3c6e92ec1c30d11d5c36f31bd228079b130b8b
Vladimir Petrov <devel@mechmind.net>
2013-03-16 14:26:57 +0700

    Rearranged Sources/EffectSystem.h for compiling on linux

--- Sources/EffectSystem.h
+++ Sources/EffectSystem.h
@@ -1,13 +1,10 @@
 #pragma once
 
 #include <vector>
+#include "constheader.h"
 
 template<typename TEffect>
-TEffect* getEffectOf()
-{
-    static EffectFabricOf<TEffect> fabric;
-    return fabric.Get();
-}
+TEffect* getEffectOf();
 
 class IFabric
 {
@@ -40,44 +37,6 @@ inline FabricProcesser* GetFabricProcesser()
 {
     return FabricProcesser::Get();
 }
-
-template<typename TEffect>
-class EffectFabricOf : public IFabric
-{
-public:
-    virtual void process() override
-    {
-        for(int i = effects_.size() - 1; i >= 0; --i)
-            if (effects_[i]->getState() == TEffect::PROCESS)
-                effects_[i]->process();
-    }
-    friend TEffect* getEffectOf<TEffect>();
-private:
-    EffectFabricOf()
-    {
-        SYSTEM_STREAM << "NewFabric!" << "\n";
-        FabricProcesser::Get()->AddFabric(this);
-        last_used_ = 0;
-        for(int i = 0; i < TEffect::PREP_SIZE; ++i)
-            effects_.push_back(new TEffect);
-    }
-    TEffect* Get()
-    {
-        int l_size = effects_.size();
-        for(int i = (last_used_ + 1) % l_size; i != last_used_; i = (++i) % l_size)
-            if(effects_[i]->getState() == BaseEffect::UNUSED)
-            {
-                last_used_ = i;
-                return effects_[i];
-            }
-        for(int i = 0; i < l_size; ++i)
-            effects_.push_back(new TEffect);
-        last_used_ = l_size + 1;
-        return effects_[last_used_];
-    }
-    int last_used_;
-    std::vector<TEffect*> effects_;
-};
 
 class BaseEffect
 {
@@ -121,3 +80,50 @@ protected:
 private:
     STATE state_;
 };
+
+template<typename TEffect>
+class EffectFabricOf : public IFabric
+{
+public:
+    virtual void process() override
+    {
+        for(int i = effects_.size() - 1; i >= 0; --i)
+            if (effects_[i]->getState() == TEffect::PROCESS)
+                effects_[i]->process();
+    }
+    friend TEffect* getEffectOf<TEffect>();
+private:
+    EffectFabricOf()
+    {
+        SYSTEM_STREAM << "NewFabric!" << "\n";
+        FabricProcesser::Get()->AddFabric(this);
+        last_used_ = 0;
+        for(int i = 0; i < TEffect::PREP_SIZE; ++i)
+            effects_.push_back(new TEffect);
+    }
+    TEffect* Get()
+    {
+        int l_size = effects_.size();
+        for(int i = (last_used_ + 1) % l_size; i != last_used_; i = (++i) % l_size)
+            if(effects_[i]->getState() == BaseEffect::UNUSED)
+            {
+                last_used_ = i;
+                return effects_[i];
+            }
+        for(int i = 0; i < l_size; ++i)
+            effects_.push_back(new TEffect);
+        last_used_ = l_size + 1;
+        return effects_[last_used_];
+    }
+    int last_used_;
+    std::vector<TEffect*> effects_;
+};
+
+template<typename TEffect>
+TEffect* getEffectOf()
+{
+    static EffectFabricOf<TEffect> fabric;
+    return fabric.Get();
+}
+
+
